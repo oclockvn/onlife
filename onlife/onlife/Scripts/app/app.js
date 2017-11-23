deb744e94fbcb947ecbb8176b953071cd67e7ff4
oclockvn <oclockvn@gmail.com>
2017-11-24 00:05:41 +0700

    added menu responsive js

---
+++ onlife/onlife/Scripts/app/app.js
@@ -0,0 +1,27 @@
+﻿var app = app || {
+
+    _init: function () {
+        this._mobileMenuButton();
+    },
+
+    _mobileMenuButton: function () {
+        $(".header").on("click", ".hamburger", function (e) {
+            e.preventDefault();
+
+            var $nav = $(this).closest(".header").find(".nav");
+            $nav.removeClass("flex-mobile");
+
+            $nav.slideToggle(250, function () {
+                if ($nav.css("display") === "none") {
+                    $nav.removeAttr("style");
+                } else {
+                    $nav.addClass("flex-mobile");
+                }
+            });
+        });
+    }
+};
+
+$(document).ready(function () {
+    app._init();
+});
