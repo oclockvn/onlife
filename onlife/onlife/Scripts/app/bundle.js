4da7054693a93a07e1e38ffc2ca04ce677e008af
oclockvn <oclockvn@gmail.com>
2017-11-24 23:27:53 +0700

    handle add bundle

--- onlife/onlife/Scripts/app/bundle.js
+++ onlife/onlife/Scripts/app/bundle.js
@@ -9,19 +9,36 @@ function Bundle(type) {
     this.id = ko.observable(0);
     this.expanded = ko.observable(false);
     this.price = ko.observable(0);
-    this.options = ko.observableArray();
+    this.options = ko.observableArray([
+        new BundleOption("data", [new OptionValues('7gb', 7, false), new OptionValues('10gb', 10, false)]),
+        new BundleOption("topup", [new OptionValues('auto', 7, false), new OptionValues('manual', 10, false)]),
+        new BundleOption("contract", [new OptionValues('12 months', 7, false), new OptionValues('monthly', 10, false)])
+    ]);
     this.type = ko.observable(type || 2); // default is mobile
+    this.includeMobileTalk = ko.observable(false);
 
     this.create = function () {
         self.id(Number(_.uniqueId()));
 
         console.log("bundle %i is created", self.id());
     };
+
+    this.toggleMobileTalk = function () {
+        self.includeMobileTalk(!self.includeMobileTalk());
+
+        console.log("bundle %s include mobile talk: %o", self.id(), self.includeMobileTalk());
+    };
 }
 
 function BundleOption(name, options) {
     this.name = ko.observable(name || "");
     this.options = ko.observableArray(options || []);
+}
+
+function OptionValues(text, price, selected) {
+    this.text = ko.observable(text || "");
+    this.price = ko.observable(price || "");
+    this.selected = ko.observable(selected || false);
 }
 
 function ViewModel() {
