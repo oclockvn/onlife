5817766ddfc601dc63523fd524259596a458bcc0
oclockvn <oclockvn@gmail.com>
2017-11-24 22:17:37 +0700

    setup ko

---
+++ onlife/onlife/Scripts/app/bundle.js
@@ -0,0 +1,23 @@
+﻿function Bundle() {
+    this.name = ko.observable();
+
+    this.price = ko.observable(0);
+    this.options = ko.observableArray();
+}
+
+function BundleOption(name, options) {
+    this.name = ko.observable(name || "");
+    this.options = ko.observableArray(options || []);
+}
+
+function ViewModel() {
+    var self = this;
+    
+    self.bundles = ko.observableArray();
+}
+
+(function () {
+    var vm = new ViewModel();
+
+    ko.applyBindings(vm);
+})();
